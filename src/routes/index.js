72e145a076d5777db1c9dd74683d6a77a4604857
Yuki Finamore <yuki@loldesign.com.br>
2020-07-24 01:06:39 -0300

    Create routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React                       from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+// Pages
+import Login 	 from '../pages/Login'
+import Profile from '../pages/Profile'
+import Feed    from '../pages/Feed'
+
+export default () => (
+  <Switch>
+    <Route exact path="/" component={Login}/>
+    <Route exact path="/profile" component={Profile}/>
+    <Route exact path="/feed" component={Feed}/>
+  </Switch>
+);
