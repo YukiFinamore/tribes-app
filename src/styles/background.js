6cc4dd02281374e7bbabfa0aa5ac3cdc0b4fd34d
Yuki Finamore <yuki@loldesign.com.br>
2020-07-25 04:08:04 -0300

    Update background styles

--- src/styles/background.js
+++ src/styles/background.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import loginBg from '../assets/images/bg/login-bg.png'
+import profileBg from '../assets/images/bg/profile-bg.png'
 
 export const Background = styled.div`
   @media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
@@ -14,5 +15,11 @@ export const Background = styled.div`
 export const LoginBg = styled(Background)`
   @media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
     background-image: url(${loginBg});
+  }
+`;
+
+export const ProfileBg = styled(Background)`
+  @media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
+    background-image: url(${profileBg});
   }
 `;
