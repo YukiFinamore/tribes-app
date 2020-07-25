30a88bc2f36e3dba6deb7e49325d0947348ac9d7
Yuki Finamore <yuki@loldesign.com.br>
2020-07-25 19:52:21 -0300

    Add default route to redirect to feed on Profile Screen

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from "../../components/Header";
 import Avatar from "../../components/Avatar";
+import { Link } from "react-router-dom";
 import { Container, AvatarContainer, CamIcon, TopicsList, ListItemContainer } from './styles'
 import { ProfileBg } from '../../styles/background'
 import profileTopicIcon from '../../assets/images/icons/profile-topic.png'
@@ -8,11 +9,13 @@ import dragIcon from '../../assets/images/icons/drag.png'
 import camIcon from '../../assets/images/icons/cam.png'
 
 const ListItem = () => (
-	<ListItemContainer>
-		<img src={profileTopicIcon}></img> 
-		<p> Lorem Ipsum </p>
-		<img src={dragIcon}></img>
-	</ListItemContainer>
+  <Link to="/feed">
+  	<ListItemContainer>
+  		<img src={profileTopicIcon}></img> 
+  		<p> Lorem Ipsum </p>
+  		<img src={dragIcon}></img>
+  	</ListItemContainer>
+  </Link>
 )
 
 export default () => (
