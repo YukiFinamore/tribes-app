1241ae90e12134e6067c1a0b77426427165e858d
Yuki Finamore <yuki@loldesign.com.br>
2020-07-25 01:33:57 -0300

    Add GeneralStyles and FontStyles to App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
 
+import GeneralStyles from './styles/general'
+import FontStyles from './styles/fonts'
+
 const App = ({}) => (
   <>
     <BrowserRouter>
+    	<GeneralStyles />
+    	<FontStyles />
       <Routes />
     </BrowserRouter>
   </>
