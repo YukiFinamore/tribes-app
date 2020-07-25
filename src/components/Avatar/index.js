7c5a6eb65a169d764aa6ffdb18e63d02cbf6597e
Yuki Finamore <yuki@loldesign.com.br>
2020-07-25 04:07:02 -0300

    Create Avatar Component

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './styles'
+import user from '../../assets/images/users/yuki.png'
+
+
+const Avatar = ({}) => (
+  <Container>
+    <img src={user}></img> 
+  </Container>
+)
+
+export default Avatar
---
+++ src/components/Avatar/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import variables from '../../styles/variables'
+
+export const Container = styled.div`
+  @media only screen and (min-device-width: 320px) and (max-device-width: 1024px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 250px;
+    height: 250px;
+    border-radius: 100px;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`
