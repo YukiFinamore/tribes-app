71652d42b5fda05c2fc4c20ff73e620bf7e96ada
Yuki Finamore <yuki@loldesign.com.br>
2020-07-25 05:24:16 -0300

    Add new variables

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,12 +1,15 @@
 const variables = {
   // Font Colors
   fontWhite: '#FFFFFF',
+  fontGray: '#BFBFBF',
 
   // Background Colors
   bgOrange: '#E57728',
   bgInputGray: '#47484C',
   bgScrollPink: '#CB28E5',
   bgScrollGray: '#9597A1',
+  bgGray: '#303134',
+  bgTopic: '#191A1D',
 }
 
 export default variables
